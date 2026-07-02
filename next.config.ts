1c10660cf872e0829c1a629b0318f5b2b1648ffa
G. Sampath Kumar <sampathgummidi@gmail.com>
2026-07-02 10:18:11 +0530

    issue Solved

--- next.config.ts
+++ next.config.ts
@@ -38,6 +38,21 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'singulariti.in',
+          },
+        ],
+        destination: 'https://www.singulariti.in/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
